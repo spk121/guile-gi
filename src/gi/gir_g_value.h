675b6d71b51bc172281c8a2b5f53eb5ed16b0200
Mike Gran <spk121@yahoo.com>
2018-10-25 05:27:51 -0700

    remove unused

--- src/gi/gir_g_value.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GIR_G_VALUE_H_
-#define _GIR_G_VALUE_H_
-
-extern SCM GuGValue_Type;
-typedef SCM SCM_GVALUE;
-int gug_value_from_scm_with_error(GValue *value, SCM obj);
-void gir_init_g_value (void);
-#endif
