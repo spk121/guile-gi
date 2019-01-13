567c3df3770695e3b32d026b87c283ee33417c94
Michael Gran <spk121@yahoo.com>
2019-01-13 13:08:26 -0800

    Add send / connect macros

--- src/gi.scm
+++ src/gi.scm
@@ -1,25 +1,41 @@
+;; Copyright (C), 2018, 2019 Michael L. Gran
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https:;;www.gnu.org/licenses/>.
 (define-module (gi)
-  #:use-module (ice-9 eval-string)
-  #:export (send))
+  #:export (send
+            connect))
+
+;; This macro derives from
+;; https:;;lists.gnu.org/archive/html/guile-user/2018-12/msg00037.html
 
 (define-syntax send
-  (syntax-rules ()
-    ((_ s ( m ))
-     (call-method s (symbol->string m)))
-    ((_ s ( m a ))
-     (call-method s (symbol->string m) a))
-    ((_ s ( m a b ))
-     (call-method s (symbol->string m) a b))
-    ((_ s ( m a b c ))
-     (call-method s (symbol->string m) a b c))
-    ((_ s ( m a b c d ))
-     (call-method s (symbol->string m) a b c d ))
-    ((_ s ( m a b c d e))
-     (call-method s (symbol->string m) a b c d e))
-    ((_ s ( m a b c d e f))
-     (call-method s (symbol->string m) a b c d e f))
-    ((_ s ( m a b c d e f g))
-     (call-method s (symbol->string m) a b c d e f g))))
+  (lambda (stx)
+    (syntax-case stx ()
+      ((_ self (method arg ...))
+       (identifier? #'method)
+       (with-syntax ((method-str (symbol->string
+                                  (syntax->datum #'method))))
+         #'(call-method self method-str arg ...))))))
+
+(define-syntax connect
+  (lambda (stx)
+    (syntax-case stx ()
+      ((_ self (method arg ...))
+       (identifier? #'method)
+       (with-syntax ((method-str (symbol->string
+                                  (syntax->datum #'method))))
+         #'(signal-connect self method-str arg ...))))))
 
 (load-extension "libguile-gi" "gir_init")
 
