8c802626726547c9e9e9e85078352f80c4d6bfe7
Michael Gran <spk121@yahoo.com>
2019-01-13 20:03:01 -0800

    add webkit library

--- src/gi/gtk-3.scm
+++ src/gi/gtk-3.scm
@@ -2,3 +2,6 @@
   #:use-module (gi))
 
 (typelib-load "Gtk" "3.0")
+;; (typelib-load "Gio" "2.0")
+;; (typelib-load "GLib" "2.0")
+
---
+++ src/gi/webkit2-4.scm
@@ -0,0 +1,5 @@
+(define-module (gi webkit2-4)
+  #:use-module (gi))
+
+(typelib-load "WebKit2" "4.0")
+
