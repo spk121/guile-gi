587b90cf35c0d59612c8e12a5592f66401eb4663
Michael Gran <spk121@yahoo.com>
2019-07-05 19:51:30 -0700

    Remove extraneous header comment

--- src/gi_giargument.c
+++ src/gi_giargument.c
@@ -1,4 +1,5 @@
 // Copyright (C) 2018, 2019 Michael L. Gran
+// Copyright (C) 2005-2009 Johan Dahlin <johan@gnome.org>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,6 +13,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <stdint.h>
 #include <glib.h>
 #include <girepository.h>
@@ -252,27 +254,6 @@ array_length(struct array_info *ai, GIArgument *arg)
         g_debug("array has length 0, are you sure about that?");
     return array_length;
 }
-
-/*
- * vim: tabstop=4 shiftwidth=4 expandtab
- *
- * Copyright (C) 2005-2009 Johan Dahlin <johan@gnome.org>
- *
- *   pygi-argument.c: GIArgument - PyObject conversion functions.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
- */
 
 static const intmax_t intmin[GI_TYPE_TAG_N_TYPES] = {
     [GI_TYPE_TAG_INT8] = INT8_MIN,
