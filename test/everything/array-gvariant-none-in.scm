2ea62b2f64aaad8f4357105a08222752d641d4ef
Michael Gran <spk121@yahoo.com>
2019-07-30 21:56:16 -0700

    #26: more garray and arra tests

---
+++ test/everything/array-gvariant-none-in.scm
@@ -0,0 +1,16 @@
+(use-modules (gi)
+             (test automake-test-lib)
+             (rnrs bytevectors)
+             (srfi srfi-43))
+
+(typelib-require ("Marshall" "1.0") ("GLib" "2.0"))
+
+(automake-test
+ (let ((v1 (variant:new-int32 27))
+       (v2 (variant:new-string "Hello")))
+   (let ((x (vector v1 v2)))
+     (format #t "Input: ~S~%" x)
+     (let ((y (array-gvariant-none-in x)))
+       (format #t "Output: ~S~%" x)
+       (and (= 27 (variant:get-int32 (vector-ref x 0)))
+            (string=? "Hello" (variant:get-string (vector-ref x 1))))))))
