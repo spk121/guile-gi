52fae3b22fd6b13bb487f4d08e221d3b6dd242da
Mike Gran <spk121@yahoo.com>
2019-08-29 18:16:25 -0700

    Skip some GTK tests when running w/o X

--- test/gtk_entry/set_alignment.scm
+++ test/gtk_entry/set_alignment.scm
@@ -4,21 +4,21 @@
 (unless (false-if-exception (require "Gtk" "3.0"))
   (exit 'EXIT_SKIPPED))
 
-(load-by-name "Gtk" "init")
+(load-by-name "Gtk" "init_check")
 (load-by-name "Gtk" "Entry")
 
 ;; This test exercises float32 argument conversions and return types.
 
 (automake-test
- (begin
-   (init)
-   (let ([entry1 (entry:new)]
-         [entry2 (entry:new)])
-     (set-alignment entry1 1.0)
-     (set-alignment entry2 0.5)
-     (format #t "entry 1: ~S~%" entry1)
-     (format #t "entry 1 alignment: ~S~%" (get-alignment entry1))
-     (format #t "entry 2: ~S~%" entry2)
-     (format #t "entry 2 alignment: ~S~%" (get-alignment entry2))
-     (and (= 1.0 (get-alignment entry1))
-          (= 0.5 (get-alignment entry2))))))
+ (if (not (init-check))
+     'skipped
+     (let ([entry1 (entry:new)]
+           [entry2 (entry:new)])
+       (set-alignment entry1 1.0)
+       (set-alignment entry2 0.5)
+       (format #t "entry 1: ~S~%" entry1)
+       (format #t "entry 1 alignment: ~S~%" (get-alignment entry1))
+       (format #t "entry 2: ~S~%" entry2)
+       (format #t "entry 2 alignment: ~S~%" (get-alignment entry2))
+       (and (= 1.0 (get-alignment entry1))
+            (= 0.5 (get-alignment entry2))))))
--- test/gtk_entry/set_alignment_zero.scm
+++ test/gtk_entry/set_alignment_zero.scm
@@ -4,15 +4,15 @@
 (unless (false-if-exception (require "Gtk" "3.0"))
   (exit 'EXIT_SKIPPED))
 
-(load-by-name "Gtk" "init")
+(load-by-name "Gtk" "init_check")
 (load-by-name "Gtk" "Entry")
 
 ;; This test exercises float32 argument conversions and return types.
 (automake-test
- (begin
-   (init)
-   (let ([entry (entry:new)])
-     (set-alignment entry 0)
-     (format #t "entry: ~S~%" entry)
-     (format #t "entry alignment: ~S~%" (get-alignment entry))
-     (= 0 (get-alignment entry)))))
+ (if (not (init-check))
+     'skipped
+     (let ([entry (entry:new)])
+       (set-alignment entry 0)
+       (format #t "entry: ~S~%" entry)
+       (format #t "entry alignment: ~S~%" (get-alignment entry))
+       (= 0 (get-alignment entry)))))
