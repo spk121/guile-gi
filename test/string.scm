c9796804131f019a620b1db29dcffe2136de05cf
LordYuuma <lordyuuma@gmail.com>
2019-09-27 18:42:28 +0200

    #63: pack conversion and unichar tests together with strings

--- test/string.scm
+++ test/string.scm
@@ -7,7 +7,7 @@
                #:renamer (protect* '(test-equal
                                      test-assert
                                      test-skip))))
-(test-begin "string.scm")
+(test-begin "strings")
 
 (let* ((str1 "hello")
        (str2 (strdup str1))
@@ -43,4 +43,48 @@
    "tokenize-and-fold ascii-alternates"
    (vector= string-ci=? #("pates" "francaises") ascii-alternates)))
 
-(test-end "string.scm")
+(test-end "strings")
+
+(test-begin "unichar")
+
+(test-eqv "unichar-combine-none"
+  0
+  (unichar-combining-class #\と))
+
+(test-eqv "unichar-combine-above"
+  230
+  (unichar-combining-class #\◌̀))
+
+;; This doesn't work because the 3rd arg of g_unichar_compose
+;; isn't registered as OUT.  See
+;; https://gitlab.gnome.org/GNOME/glib/issues/1811
+(test-skip "unichar-compose")
+(test-assert "unichar-compose"
+  ;; (receive (success out)
+  ;;     (unichar-compose #\A #\◌́)
+  ;;   (format #t "Output: ~S~%" x)
+  ;;   (format #t "Success: ~S~%" success)
+  ;;   (and success
+  ;;        (char=? ch Á)))
+  #f)
+
+(test-assert "unichar-isalpha"
+ (unichar-isalpha? #\と))
+
+(test-assert "unichar-validate-zero"
+ (unichar-validate? #\x0000))
+
+(test-end "unichar")
+
+(unless (< MINOR_VERSION 56)
+  (test-begin "conversions")
+
+  (test-equal "convert-latin-1"
+    #u8(195 129 195 137)
+    (convert #u8(193 201) "UTF-8" "ISO-8859-1"))
+
+  (test-equal "convert-ascii"
+    #u8(65 66 67 68 69)
+    (convert #u8(65 66 67 68 69) "UTF-8" "US-ASCII"))
+  
+  (test-end "conversions"))
