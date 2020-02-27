99c6028cea674a4e088d3915ece0bac13db32939
LordYuuma <lordyuuma@gmail.com>
2020-02-27 16:34:22 +0100

    meson: add configuration and install scheme code

---
+++ meson_postconf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p $MESON_BUILD_ROOT/tools
+for tool in 'gdb-guile' 'gdb-test' 'lcov.sh' 'run-guile' 'run-test' 'uninstalled-env' 'uninstalled-test-env' 'valgrind.sh'; do
+    mv $MESON_BUILD_ROOT/$tool $MESON_BUILD_ROOT/tools/$tool
+done
+mv $MESON_BUILD_ROOT/config.scm $MESON_SOURCE_ROOT/module/gi/config.scm
