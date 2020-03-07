86c7ca6b19d4c1b50799a13a58252d3c8f07d053
LordYuuma <lordyuuma@gmail.com>
2020-03-07 12:41:33 +0100

    update postconf script

--- meson/postconf.sh
+++ meson/postconf.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-mkdir -p $MESON_BUILD_ROOT/tools
-mkdir -p $MESON_BUILD_ROOT/test
-for tool in 'gdb-guile' 'gdb-test' 'lcov.sh' 'run-guile' 'run-test' 'uninstalled-env' 'uninstalled-test-env' 'valgrind.sh'; do
-    mv $MESON_BUILD_ROOT/$tool $MESON_BUILD_ROOT/tools/$tool
-done
-mv $MESON_BUILD_ROOT/config.scm $MESON_SOURCE_ROOT/module/gi/config.scm
+mkdir -p $MESON_BUILD_ROOT/test # required for tests
+chmod +w $MESON_BUILD_ROOT/gitestmacros.h # required on Guix
+mv $MESON_BUILD_ROOT/config.scm $MESON_SOURCE_ROOT/module/gi/config.scm # required for build
