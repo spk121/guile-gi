4b4dbf88e169300de1cb3883e753ac448d1a6754
Michael Gran <spk121@yahoo.com>
2022-04-04 16:11:22 -0700

    Add more customization options to gi-parse script

--- module/scripts/gi-parse.scm
+++ module/scripts/gi-parse.scm
@@ -20,7 +20,42 @@
 ;; This program creates modules for Guile-GI bindings to libraries,
 ;; that use GObject introspection.
 ;;
-;; Example: gi-parse -n GLib -v 2.0
+;; The simplest way to use this is to create a standard module for a
+;; given top-level namespace.  The following creates an import library
+;; for Gtk 3.0.  This file can be used with `use-modules`.  It may be
+;; very large and take a significant amount of time to compile and
+;; run.
+;;
+;; Example: gi-parse --namespace=Gtk --version=3.0
+;;
+;; But, since modules have dependencies, you'll have to create the
+;; modules for the dependencies as well.  This can be done
+;; automatically using the `--recurse` option.
+;;
+;; Example: gi-parse --namespace=Gtk --version=3.0 --recurse
+;;
+;; Alternately, this tool can create header-less files (without the
+;; `define-module` header) that can be used with 'load' instead of
+;; `use-modules`.
+;;
+;; Example: gi-parse --namespace=Gtk --version=3.0 --no-header
+;;
+;; Dependencies matter here as well.  If you want to create a full set
+;; of headerless files, use the `--recurse` options.
+;;
+;; To create much smaller libraries, the tool can be passed a list of
+;; specified symbols to extract. These symbols are either function
+;; names like `gtk_window_new` or type names like `GtkWindow`. The
+;; symbols can be passed in on the command-line using the `--symbols`
+;; argument or can be taken from a files using the `--symbols-file`
+;; argument.  The tool will also extract the minimal set of
+;; dependencies required for those functions and type to be imported.
+;;
+;; Example: gi-parse --namespace=Gtk --version=3.0 --symbols="GtkWindow"
+;;
+;; To see the list of symbols in a given namespace, you can use the `--dump`
+;; option.  The `--dump` option will not create libraries.
+;;
 ;;
 ;; Required options are:
 ;;
@@ -36,6 +71,16 @@
 ;;                               what you want.
 ;;   -R, --recurse               Also create modules for all the dependencies
 ;;                               of the given namespace.
+;;   -H, --no-header             Don't add the 'define-module' header to
+;;                               the files.
+;;   --symbols="LIST"            A comma or whitespace separated list of
+;;                               import symbols. Only import these symbols
+;;                               and the necessary predecessors.
+;;   -S, --symbol-file=FILENAME  Like --symbols, but read from a file.
+;;   -t, --top-level=NAME        For modules with 'define-module' headers, this
+;;                               sets the top-level to be NAME instead of `gi`.
+;;   -d, --dump                  List the symbols in the given namespace.
+;;                               Overrides all other output options.
 
 ;;; Code:
 
@@ -58,7 +103,12 @@
     (output    (single-char #\o) (value #t))
     (force     (single-char #\f) (value #f))
     (recurse   (single-char #\R) (value #f))
-    (help      (single-char #\h))))
+    (no-header (single-char #\H) (value #f))
+    (top-level (single-char #\t) (value #t))
+    (symbols                     (value #t))
+    (symbol-file (single-char #\S) (value #t))
+    (dump      (single-char #\d) (value #f))
+    (help      (single-char #\h) (value #f))))
 
 (define (display-help)
   (display
@@ -81,20 +131,52 @@
       (else c)))
    str))
 
-(define (header port lib)
+(define (wrap-print lst port)
+  (let loop ((cur (car lst))
+             (rest (cdr lst))
+             (i 10))
+    (if (and (> (+ (string-length cur) i) 78)
+             (< (string-length cur) 50))
+        (begin
+          (newline port)
+          (display "           " port)
+          (loop cur rest 10))
+        ;; else
+        (begin
+          (display cur port)
+          (unless (null? rest)
+            (display " " port)
+            (loop (car rest) (cdr rest) (+ i 1 (string-length cur))))))))
+
+(define* (header port lib #:key (dependencies? #t) (subs '()) (re-exports '()))
   (display "(define-module (gi " port)
   (display (safename (string-append (first lib) "-" (second lib))) port)
   (display ")" port)
   (newline port)
   (display "  #:use-module (gi runtime)" port)
   (newline port)
-  (for-each
-   (lambda (dep)
-     (display "  #:use-module (gi " port)
-     (display (safename dep) port)
-     (display ")" port)
-     (newline port))
-   (namespace-immediate-dependencies (first lib))))
+  (unless (null? subs)
+    (for-each
+     (lambda (sub)
+       (display "  #:use-module (gi " port)
+       (display (safename sub) port)
+       (display ")" port)
+       (newline port))
+     subs))
+  (when dependencies?
+    (for-each
+     (lambda (dep)
+       (display "  #:use-module (gi " port)
+       (display (safename dep) port)
+       (display ")" port)
+       (newline port))
+     (namespace-immediate-dependencies (first lib))))
+  (unless (null? re-exports)
+    (lambda (sub)
+      (display "  #:re-export(" port)
+      (wrap-print subs port)
+      (display ")" port)
+      (newline port))))
 
 
 ;; This is not the way to do this; the IL functions return the symbols
@@ -133,24 +215,15 @@
      (else
       '()))))
 
-(define (wrap-print lst port)
-  (let loop ((cur (car lst))
-             (rest (cdr lst))
-             (i 10))
-    (if (and (> (+ (string-length cur) i) 78)
-             (< (string-length cur) 50))
-        (begin
-          (newline port)
-          (display "           " port)
-          (loop cur rest 10))
-        ;; else
-        (begin
-          (display cur port)
-          (unless (null? rest)
-            (display " " port)
-            (loop (car rest) (cdr rest) (+ i 1 (string-length cur))))))))
+(define (dump lib)
+  (namespace-load (first lib) (second lib))
+  (let ((syms (map info-name (namespace-infos (first lib)))))
+    (for-each
+     (lambda (sym)
+       (display sym) (newline))
+     (sort syms string<?))))
 
-(define (contents port lib)
+(define (contents port lib info-names header?)
   (let ((p (new-parser))
         (exports '())
         (str (open-output-string)))
@@ -158,7 +231,12 @@
       (lambda ()
         (parser-add-namespace! p (first lib) (second lib))
         (for-each (lambda (i)
-                    (parser-add-info! p i LOAD_EVERYTHING))
+                    (if (null? info-names)
+                        (parser-add-info! p i LOAD_EVERYTHING)
+                        ;; else
+                        (when (member (info-name i) info-names)
+                          ;; FIXME: we could handle regex symbols, perhaps.
+                          (parser-add-info! p i LOAD_EVERYTHING))))
                   (namespace-infos (first lib)))
         (let ((IL (parser-take-output! p #t)))
           (for-each
@@ -167,24 +245,28 @@
              (pretty-print i str)
              (newline str))
            IL))
-        (display "  #:export(" port)
-        (wrap-print (reverse exports) port)
-        (display "))" port)
-        (newline port)
-        (newline port)
-        (display "(eval-when (expand load eval)" port)
-        (newline port)
+        (when header?
+          (display "  #:export(" port)
+          (wrap-print (reverse exports) port)
+          (display "))" port)
+          (newline port)
+          (newline port)
+          (display "(eval-when (expand load eval)" port)
+          (newline port))
         (display (get-output-string str) port)
         (newline port)
-        (display ")" port)
-        (newline port))
+        (when header?
+          (display ")" port)
+          (newline port)))
       (lambda x
+        (display ")" port)
+        (newline port)
         (display ";;; No typelib info found for this namespace" port)
         (newline port)
         (write x)
         (newline)))))
 
-(define (makelib lib path overwrite)
+(define (makelib lib path info-names overwrite no-header?)
   (let ((namespace (first lib))
         (version (second lib)))
     (let ((filename (string-append
@@ -198,8 +280,9 @@
             #f)
           ;; else
           (let ((port (open-file filename "w")))
-            (header port lib)
-            (contents port lib)
+            (unless no-header?
+              (header port lib))
+            (contents port lib info-names (not no-header?))
             (close port))))))
 
 (define (parse . args)
@@ -208,7 +291,12 @@
           (namespace (option-ref p 'namespace #f))
           (version (option-ref p 'version #f))
           (overwrite? (option-ref p 'force #f))
-          (recurse? (option-ref p 'recurse #f)))
+          (recurse? (option-ref p 'recurse #f))
+          (no-header? (option-ref p 'no-header #f))
+          (symbol-str (option-ref p 'symbols #f))
+          (symbol-file (option-ref p 'symbol-file #f))
+          (dump? (option-ref p 'dump #f))
+          (info-names '()))
       (cond
        (help-wanted?
         (display-help))
@@ -216,6 +304,29 @@
         (if version
             (namespace-load namespace version)
             (namespace-load namespace))
+        (when dump?
+          (dump (list namespace (namespace-version namespace)))
+          (exit EXIT_SUCCESS))
+        (when symbol-str
+          (set! info-names
+            (string-split symbol-str
+                          (->char-set " ,;"))))
+        (when symbol-file
+          (if (file-exists? symbol-file)
+              (begin
+                (with-input-from-file symbol-file
+                  (while #t
+                    (let ((cur (string-trim-both (read-delimited " ,;\t\r\n"))))
+                      (if (eof-object? cur)
+                          (break)
+                          ;; else
+                          (unless (string-null? cur)
+                            (set! info-names (cons cur info-names))))))))
+              ;; else
+              (begin
+                (format (current-error-port)
+                        "Cannot open symbol file ~A" symbol-file)
+                (exit EXIT_FAILURE))))
         (let* ((outdir (or
                         (option-ref p 'output #f)
                         (false-if-exception
@@ -233,8 +344,10 @@
                (libs (cons (list namespace (namespace-version namespace))
                            dependencies)))
           (for-each (lambda (lib)
-                      (makelib lib outdir overwrite?))
+                      (makelib lib outdir info-names overwrite? no-header?))
                     libs)
-          (exit EXIT_SUCCESS)))))))
+      )))
+      #t)
+    (exit EXIT_SUCCESS)))
 
 (define main parse)
